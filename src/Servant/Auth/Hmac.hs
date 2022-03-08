413f68d81b7b75e0faf2cb809d2df21e374174f1
Tuan Le <webmaster@michivi.com>
2022-03-08 14:45:49 +0100

    Format using fourmolu

--- src/Servant/Auth/Hmac.hs
+++ src/Servant/Auth/Hmac.hs
@@ -1,10 +1,9 @@
 {- | Servant authentication with HMAC. Contains server and client
 implementation.
 -}
-
-module Servant.Auth.Hmac
-       ( module Hmac
-       ) where
+module Servant.Auth.Hmac (
+    module Hmac,
+) where
 
 import Servant.Auth.Hmac.Client as Hmac
 import Servant.Auth.Hmac.Crypto as Hmac
