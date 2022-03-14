abcf3207a9703e0bfdc759406819417e6fd6cc6f
Tuan Le <webmaster@michivi.com>
2022-03-14 19:18:07 +0100

    Add options to identify the user

--- src/Servant/Auth/Hmac/Secure.hs
+++ src/Servant/Auth/Hmac/Secure.hs
@@ -1,15 +1,17 @@
-{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
 
 {- |
 Servant combinator and functions for HMAC authentication of requests.
 -}
 module Servant.Auth.Hmac.Secure (
-    -- * Servant combinator and context
+    -- * Servant combinator
     HmacAuthed,
-    HmacAuthCheck (..),
-    hmacAuthCheckContext,
+
+    -- * HMAC server
+    HmacServerSideAuth (..),
 
     -- * HMAC client
+    HmacClientSideAuth (..),
     hmacClient,
     runHmacClient,
 
@@ -23,11 +25,12 @@ import Control.Monad.Except (MonadError (throwError))
 import Control.Monad.IO.Class (MonadIO (..))
 import Control.Monad.Reader (MonadReader (ask), ReaderT (ReaderT, runReaderT), asks)
 import Data.ByteString (ByteString)
+import Data.Kind (Type)
 import Data.Sequence ((|>))
 import qualified Data.Sequence as Seq
-import Network.HTTP.Types (HeaderName)
-import Network.HTTP.Types.Header (hHost)
-import Servant (Context (EmptyContext, (:.)), HasContextEntry (getContextEntry), HasLink (MkLink, toLink), HasServer (ServerT), Proxy (Proxy), ServerError (errBody), err401, type (:>))
+import Network.HTTP.Types.Header (HeaderName, hHost)
+import qualified Network.Wai as Wai
+import Servant (HasContextEntry (getContextEntry), HasLink (MkLink, toLink), HasServer (ServerT), Proxy (Proxy), ServerError (errBody), err401, type (:>))
 import Servant.Auth.Hmac.Crypto (SecretKey, Signature (..), requestSignature, verifySignatureHmac)
 import Servant.Auth.Hmac.Internal (normalizedHostFromUrl, servantDuplicateRequestBody, servantRequestToPayload, waiDuplicateRequestBody, waiRequestToPayload)
 import Servant.Client (BaseUrl, ClientEnv (baseUrl), ClientError (ConnectionError), ClientM, HasClient (Client, hoistClientMonad), runClientM)
@@ -36,24 +39,45 @@ import qualified Servant.Client.Core as Servant
 import Servant.Client.Core.HasClient (HasClient (clientWithRoute))
 import Servant.Client.Core.RunClient (RunClient (runRequestAcceptStatus, throwClientError))
 import Servant.Server (HasServer (hoistServerWithContext, route))
+import Servant.Server.Internal.Delayed (addAuthCheck)
+import Servant.Server.Internal.DelayedIO (delayedFailFatal, withRequest)
 import Servant.Server.Internal.RouteResult (RouteResult (FailFatal))
 import Servant.Server.Internal.RoutingApplication (RoutingApplication)
 
 -- | Potential error during the HMAC authentication generation or verification.
 newtype HmacSignatureException
-    = -- | The HMAC signature generation has failed during the request's body hashing.
-      RequestBodyHashingFailed String
+    = -- | The HMAC signature generation has failed during the request's body generation.
+      RequestBodyInspectionFailed String
     deriving stock (Eq, Show)
 
 instance Exception HmacSignatureException
 
-{- HMAC authentication contextual information. -}
-data HmacAuthCheck = HmacAuthCheck
-    { hacSign :: !(SecretKey -> ByteString -> Signature)
+{- Client-side HMAC authentication contextual information. -}
+data HmacClientSideAuth usr = HmacClientSideAuth
+    { hcsaSign :: !(SecretKey -> ByteString -> Signature)
     -- ^ Signing algorithm used for the authentication, taking in both the
     -- secret and the request payload. The usual algorithm is 'signSHA256'.
-    , hacSecretKey :: !SecretKey
-    -- ^ Secret key used for the authentication.
+    , hcsaUserRequest :: !(usr -> Servant.Request -> IO Servant.Request)
+    -- ^ Update the request to include the user (if necessary)
+    , hcsaSecretKey :: !(usr -> SecretKey)
+    -- ^ User secret key used for the authentication.
+    }
+
+{- Server-side HMAC authentication contextual information. -}
+data HmacServerSideAuth usr = HmacServerSideAuth
+    { hssaSign :: !(SecretKey -> ByteString -> Signature)
+    -- ^ Signing algorithm used for the authentication, taking in both the
+    -- secret and the request payload. The usual algorithm is 'signSHA256'.
+    , hssaIdentifyUser :: !(Wai.Request -> IO (Maybe usr))
+    -- ^ Identify the user from the incoming request. This is required to
+    -- identify which secret key to use for authentication. The function only
+    -- tries and identifies the user from the request. The authentication
+    -- itself will be performed by the library using the secret key returned
+    -- by 'hssaSecretKey'. May return 'Nothing' if the user could not be
+    -- identified.
+    , hssaSecretKey :: !(usr -> SecretKey)
+    -- ^ Generate the secret key to use for the authentication.
+    --  The secret key may change depending on the remote user.
     }
 
 {- | The HMAC authentication combinator.
@@ -70,29 +94,31 @@ the content. The later is still required though by the backend for consumption.
 To that end, it will be retained in-memory. Users need to keep that in mind, and
 take the necessary precautions to prevent any DoS attacks by throwing very large
 payloads to the protected endpoint.
--}
-data HmacAuthed
 
-instance HasLink sub => HasLink (HmacAuthed :> sub) where
-    type MkLink (HmacAuthed :> sub) r = MkLink sub r
+'usr' is the type of user that may be authenticated through HMAC.
+-}
+data HmacAuthed (usr :: Type)
+
+instance HasLink sub => HasLink (HmacAuthed usr :> sub) where
+    type MkLink (HmacAuthed usr :> sub) r = MkLink sub r
     toLink toA _ = toLink toA (Proxy @sub)
 
-newtype HmacClientM a = HmacClientM
-    {unHmacClientM :: ReaderT HmacAuthCheck ClientM a}
-    deriving (Functor, Applicative, Monad, MonadError ClientError, MonadIO, MonadReader HmacAuthCheck)
+newtype HmacClientM usr a = HmacClientM
+    {unHmacClientM :: ReaderT (HmacClientSideAuth usr, usr) ClientM a}
+    deriving (Functor, Applicative, Monad, MonadError ClientError, MonadIO, MonadReader (HmacClientSideAuth usr, usr))
 
-instance RunClient HmacClientM where
+instance RunClient (HmacClientM usr) where
     runRequestAcceptStatus st = signOnMark >=> liftClient . runRequestAcceptStatus st
     throwClientError = liftClient . throwClientError
 
-liftClient :: ClientM a -> HmacClientM a
+liftClient :: ClientM a -> HmacClientM usr a
 liftClient = HmacClientM . ReaderT . const
 
-hmacClient :: forall api. HasClient HmacClientM api => Proxy api -> Client HmacClientM api
-hmacClient _ = Proxy @api `clientIn` Proxy @HmacClientM
+hmacClient :: forall api usr. HasClient (HmacClientM usr) api => Proxy api -> Client (HmacClientM usr) api
+hmacClient _ = Proxy @api `clientIn` Proxy @(HmacClientM usr)
 
-runHmacClient :: HmacClientM a -> ClientEnv -> HmacAuthCheck -> IO (Either ClientError a)
-runHmacClient ma env hac = runClientM (runReaderT (unHmacClientM ma) hac) env
+runHmacClient :: HmacClientM usr a -> ClientEnv -> HmacClientSideAuth usr -> usr -> IO (Either ClientError a)
+runHmacClient ma env hac usr = runClientM (runReaderT (unHmacClientM ma) (hac, usr)) env
 
 hAuthentication :: HeaderName
 hAuthentication = "Authentication"
@@ -118,26 +144,29 @@ completeRequest url req =
                 }
 
 -- | HMAC-Sign the request if it has been marked for it.
-signOnMark :: Servant.Request -> HmacClientM Servant.Request
-signOnMark req = case findAuthenticationHeader (Servant.requestHeaders req) of
-    Nothing ->
-        pure req
-    Just authHeaderIdx -> do
-        url <- liftClient $ asks baseUrl
-        HmacAuthCheck signer sk <- ask
-        let completedReq = completeRequest url req
-        x <- servantDuplicateRequestBody completedReq
-        case x of
-            Left err -> throwError . ConnectionError . SomeException $ RequestBodyHashingFailed err
-            Right (req', body) -> do
-                let payload = servantRequestToPayload url body (completedReq{requestHeaders = Seq.deleteAt authHeaderIdx (requestHeaders completedReq)})
-                let signature = requestSignature signer sk payload
-                pure $ req'{requestHeaders = Seq.update authHeaderIdx (hAuthentication, hmacAuthenticationHeaderValue signature) (requestHeaders req')}
+signOnMark :: Servant.Request -> HmacClientM usr Servant.Request
+signOnMark req = do
+    (HmacClientSideAuth signer userReq userSk, usr) <- ask
+    url <- liftClient $ asks baseUrl
+    completedReq <- completeRequest url <$> liftIO (userReq usr req)
+
+    case findAuthenticationHeader (Servant.requestHeaders completedReq) of
+        Nothing ->
+            pure completedReq
+        Just authHeaderIdx -> do
+            let sk = userSk usr
+            x <- servantDuplicateRequestBody completedReq
+            case x of
+                Left err -> throwError . ConnectionError . SomeException $ RequestBodyInspectionFailed err
+                Right (req', body) -> do
+                    let payload = servantRequestToPayload url body (completedReq{requestHeaders = Seq.deleteAt authHeaderIdx (requestHeaders completedReq)})
+                    let signature = requestSignature signer sk payload
+                    pure $ req'{requestHeaders = Seq.update authHeaderIdx (hAuthentication, hmacAuthenticationHeaderValue signature) (requestHeaders req')}
   where
     findAuthenticationHeader = Seq.elemIndexL (hAuthentication, hmacMarkValue)
 
-instance (HasClient m api) => HasClient m (HmacAuthed :> api) where
-    type Client m (HmacAuthed :> api) = Client m api
+instance (HasClient m api) => HasClient m (HmacAuthed usr :> api) where
+    type Client m (HmacAuthed usr :> api) = Client m api
 
     clientWithRoute mp _ = clientWithRoute mp (Proxy @api) . markRequestForSignature
       where
@@ -145,29 +174,30 @@ instance (HasClient m api) => HasClient m (HmacAuthed :> api) where
 
     hoistClientMonad mp _ f cl = hoistClientMonad mp (Proxy @api) f cl
 
-instance (HasServer api context, HasContextEntry context HmacAuthCheck) => HasServer (HmacAuthed :> api) context where
-    type ServerT (HmacAuthed :> api) m = ServerT api m
+instance (HasServer api context, HasContextEntry context (HmacServerSideAuth usr)) => HasServer (HmacAuthed usr :> api) context where
+    type ServerT (HmacAuthed usr :> api) m = usr -> ServerT api m
 
-    route _ context = fmap (verifyIfAuthed hmacAuthCheck) . route (Proxy @api) context
+    route _ context subserver = verifyIfAuthed hmacSsAuth <$> route (Proxy @api) context (subserver `addAuthCheck` userIdentification)
       where
-        hmacAuthCheck = getContextEntry context
+        hmacSsAuth = getContextEntry context
 
-    hoistServerWithContext _ cp nt s = hoistServerWithContext (Proxy @api) cp nt s
+        userIdentification = withRequest $ \req -> do
+            usr' <- liftIO (hssaIdentifyUser hmacSsAuth req)
+            case usr' of
+                Nothing -> delayedFailFatal err401
+                Just usr -> pure usr
 
-verifyIfAuthed :: HmacAuthCheck -> RoutingApplication -> RoutingApplication
-verifyIfAuthed (HmacAuthCheck hmacSigner hmacSk) app req respond = do
+    hoistServerWithContext _ cp nt s = hoistServerWithContext (Proxy @api) cp nt . s
+
+verifyIfAuthed :: HmacServerSideAuth usr -> RoutingApplication -> RoutingApplication
+verifyIfAuthed (HmacServerSideAuth signer reqUser userSk) app req respond = do
     (req', body) <- waiDuplicateRequestBody req
     let payload = waiRequestToPayload req body
-    let verification = verifySignatureHmac hmacSigner hmacSk payload
-    case verification of
-        Nothing -> app req' respond
-        Just bs -> respond $ FailFatal err401{errBody = bs}
-
--- | Create a HMAC request for HMAC authentication.
-hmacAuthCheckContext ::
-    -- | Signing function
-    (SecretKey -> ByteString -> Signature) ->
-    -- | Secret key that was used for signing 'Request'
-    SecretKey ->
-    Context '[HmacAuthCheck]
-hmacAuthCheckContext signer sk = HmacAuthCheck signer sk :. EmptyContext
+    usr' <- liftIO $ reqUser req'
+    case usr' of
+        Nothing ->
+            respond $ FailFatal err401
+        Just usr ->
+            case verifySignatureHmac signer (userSk usr) payload of
+                Nothing -> app req' respond
+                Just bs -> respond $ FailFatal err401{errBody = bs}
--- test/Servant/Auth/Hmac/SecureSpec.hs
+++ test/Servant/Auth/Hmac/SecureSpec.hs
@@ -4,53 +4,64 @@ import Control.Exception (SomeException (SomeException))
 import Control.Monad.IO.Class (MonadIO (liftIO))
 import Control.Monad.Trans.Except (runExceptT)
 import Data.Either (fromRight)
+import Data.List (intercalate)
 import Data.String (fromString)
 import Network.HTTP.Client hiding (Proxy)
 import Network.HTTP.Types (Status, unauthorized401)
+import qualified Network.Wai as Wai
 import qualified Network.Wai.Handler.Warp as Warp
 import Servant
 import Servant.Auth.Hmac (SecretKey (..), signSHA256)
 import Servant.Auth.Hmac.Secure
 import Servant.Client
+import qualified Servant.Client.Core as Client
 import Servant.Types.SourceT (StepT (Error), fromStepT, runSourceT, source)
 import Test.Hspec
 
 type TestApi = HelloEndpoint :<|> StreamingEndpoint
 type UnsecuredApi = UnsecuredHelloEndpoint
 
-type HelloEndpoint = HmacAuthed :> UnsecuredHelloEndpoint
+data HelloUser
+    = UserA
+    | UserB
+    deriving stock (Show)
+
+type HelloEndpoint = HmacAuthed HelloUser :> UnsecuredHelloEndpoint
 type UnsecuredHelloEndpoint = "hello" :> Capture "name" String :> QueryFlag "bye" :> ReqBody '[PlainText] String :> Post '[PlainText] String
 
-type StreamingEndpoint = HmacAuthed :> UnsecuredStreamingEndpoint
+type StreamingEndpoint = HmacAuthed () :> UnsecuredStreamingEndpoint
 type UnsecuredStreamingEndpoint = "stream" :> StreamBody NewlineFraming PlainText (SourceIO String) :> Post '[PlainText] String
 
-hello :: String -> Bool -> String -> Handler String
-hello name bye = pure . (prefix <>) . reverse
+helloImpl :: Maybe HelloUser -> String -> Bool -> String -> Handler String
+helloImpl usr name bye = pure . (prefix <>) . reverse
   where
     prefix =
         if bye
-            then "bye: "
-            else "hello from " <> name <> ": "
+            then "bye from " <> show usr <> ": "
+            else "hello from " <> show usr <> " to " <> name <> ": "
 
-stream :: SourceIO String -> Handler String
-stream strsIO = do
+hello :: HelloUser -> String -> Bool -> String -> Handler String
+hello usr = helloImpl (Just usr)
+
+stream :: () -> SourceIO String -> Handler String
+stream _ strsIO = do
     strs <- liftIO . runExceptT $ runSourceT strsIO
-    pure . show . length . filter (not . null) $ fromRight [] strs
+    pure . intercalate "," . filter (not . null) $ fromRight [] strs
 
 testApi :: Server TestApi
 testApi = hello :<|> stream
 
 unsecuredTestApi :: Server UnsecuredApi
-unsecuredTestApi = hello
+unsecuredTestApi = helloImpl Nothing
 
-testApp :: SecretKey -> Application
-testApp sk = serveWithContext (Proxy @TestApi) (hmacAuthCheckContext signSHA256 sk) testApi
+testApp :: HmacServerSideAuth HelloUser -> HmacServerSideAuth () -> Application
+testApp helloUserCtx unitUserCtx = serveWithContext (Proxy @TestApi) (helloUserCtx :. unitUserCtx :. EmptyContext) testApi
 
 unsecuredTestApp :: Application
 unsecuredTestApp = serve (Proxy @UnsecuredApi) unsecuredTestApi
 
-withTestApp :: SecretKey -> (Warp.Port -> IO ()) -> IO ()
-withTestApp sk = Warp.testWithApplication (pure $ testApp sk)
+withTestApp :: HmacServerSideAuth HelloUser -> HmacServerSideAuth () -> (Warp.Port -> IO ()) -> IO ()
+withTestApp helloUserCtx unitUserCtx = Warp.testWithApplication (pure $ testApp helloUserCtx unitUserCtx)
 
 withUnsecuredTestApp :: (Warp.Port -> IO ()) -> IO ()
 withUnsecuredTestApp = Warp.testWithApplication (pure unsecuredTestApp)
@@ -79,32 +90,64 @@ spec = do
             apiLink (Proxy @HelloEndpoint) "tom" True `shouldBeLink` "hello/tom?bye"
 
     describe "Client / Server" $ do
-        let sk = SecretKey "abc123456"
-        let anotherSk = SecretKey "something-else"
-
         baseUrl <- runIO $ parseBaseUrl "http://localhost"
         manager <- runIO $ newManager defaultManagerSettings
         let clientEnv port = mkClientEnv manager (baseUrl{baseUrlPort = port})
 
-        context "With a secured server" $
-            around (withTestApp sk) $ do
-                let hmacAuthCheck = HmacAuthCheck signSHA256 sk
-                let anotherHmacAuthCheck = HmacAuthCheck signSHA256 anotherSk
+        let userASk = SecretKey "abc123456"
+        let userBSk = SecretKey "something-else"
+        let unitSk = SecretKey "42"
+        let fooUserHeader = "foo-user"
+        let helloCtx =
+                HmacServerSideAuth
+                    { hssaSign = signSHA256
+                    , hssaIdentifyUser = \req -> case lookup fooUserHeader (Wai.requestHeaders req) of
+                        Just "user-a" -> pure $ Just UserA
+                        Just "user-b" -> pure $ Just UserB
+                        _ -> pure Nothing
+                    , hssaSecretKey = \case
+                        UserA -> userASk
+                        UserB -> userBSk
+                    }
+        let unitCtx =
+                HmacServerSideAuth
+                    { hssaSign = signSHA256
+                    , hssaIdentifyUser = const . pure $ Just ()
+                    , hssaSecretKey = const unitSk
+                    }
+        let hmacHelloClientAuth =
+                HmacClientSideAuth
+                    { hcsaSign = signSHA256
+                    , hcsaUserRequest = \case
+                        UserA -> pure . Client.addHeader fooUserHeader ("user-a" :: String)
+                        UserB -> pure . Client.addHeader fooUserHeader ("user-b" :: String)
+                    , hcsaSecretKey = \case
+                        UserA -> userASk
+                        UserB -> userBSk
+                    }
+        let hmacUnitClientAuth =
+                HmacClientSideAuth
+                    { hcsaSign = signSHA256
+                    , hcsaUserRequest = const pure
+                    , hcsaSecretKey = const unitSk
+                    }
 
+        context "With a secured server" $ do
+            around (withTestApp helloCtx unitCtx) $ do
                 let helloClient = hmacClient (Proxy @HelloEndpoint)
                 let streamClient = hmacClient (Proxy @StreamingEndpoint)
                 let unsecuredHelloClient = client (Proxy @UnsecuredHelloEndpoint)
 
                 it "should have the server authenticate the client's secured request with the same secret" $ \port -> do
-                    result <- runHmacClient (helloClient "world" False "abcdef123789") (clientEnv port) hmacAuthCheck
-                    result `shouldBeASuccessWith` "hello from world: 987321fedcba"
+                    result <- runHmacClient (helloClient "world" False "abcdef123789") (clientEnv port) hmacHelloClientAuth UserB
+                    result `shouldBeASuccessWith` "hello from Just UserB to world: 987321fedcba"
 
                 it "should have the server authenticate the client's secured streaming request with the same secret" $ \port -> do
-                    result <- runHmacClient (streamClient (source ["abc", "", "123", "42", ""] :: SourceIO String)) (clientEnv port) hmacAuthCheck
-                    result `shouldBeASuccessWith` "3"
+                    result <- runHmacClient (streamClient (source ["abc", "", "123", "42", ""] :: SourceIO String)) (clientEnv port) hmacUnitClientAuth ()
+                    result `shouldBeASuccessWith` "abc,123,42"
 
                 it "should have the server reject the client's secured request with a different secret and respond with a 401 status code" $ \port -> do
-                    result <- runHmacClient (helloClient "you" True "jdkmsqjdl") (clientEnv port) anotherHmacAuthCheck
+                    result <- runHmacClient (helloClient "you" True "jdkmsqjdl") (clientEnv port) hmacUnitClientAuth ()
                     result `shouldBeAFailureWithStatus` unauthorized401
 
                 it "should have the server reject the client's unsecured request and respond with a 401 status code" $ \port -> do
@@ -112,14 +155,13 @@ spec = do
                     result `shouldBeAFailureWithStatus` unauthorized401
 
                 it "should have the HMAC signing failed before connection when the streaming request has failed during signing" $ \port -> do
-                    result <- runHmacClient (streamClient (source ["hello", "", ""] <> fromStepT (Error "Failed!") :: SourceIO String)) (clientEnv port) hmacAuthCheck
-                    result `shouldBe` Left (ConnectionError . SomeException $ RequestBodyHashingFailed "Failed!")
+                    result <- runHmacClient (streamClient (source ["hello", "", ""] <> fromStepT (Error "Failed!") :: SourceIO String)) (clientEnv port) hmacUnitClientAuth ()
+                    result `shouldBe` Left (ConnectionError . SomeException $ RequestBodyInspectionFailed "Failed!")
 
         context "With an unsecured server" $ do
             around withUnsecuredTestApp $ do
-                let hmacAuthCheck = HmacAuthCheck signSHA256 sk
                 let helloClient = hmacClient (Proxy @HelloEndpoint)
 
                 it "should have the server respond to the client and ignore the client request's HMAC signature" $ \port -> do
-                    result <- runHmacClient (helloClient "world" True "abcdef123789") (clientEnv port) hmacAuthCheck
-                    result `shouldBeASuccessWith` "bye: 987321fedcba"
+                    result <- runHmacClient (helloClient "world" True "abcdef123789") (clientEnv port) hmacHelloClientAuth UserA
+                    result `shouldBeASuccessWith` "bye from Nothing: 987321fedcba"
